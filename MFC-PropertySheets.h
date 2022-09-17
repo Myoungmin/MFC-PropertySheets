c315f734e17939adaa0347e3b6cd44ab8fd506cd
Myoungmin <miung222@gmail.com>
2022-09-18 00:26:49 +0900

    Add project files.

---
+++ MFC-PropertySheets.h
@@ -0,0 +1,32 @@
+﻿
+// MFC-PropertySheets.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFCPropertySheetsApp:
+// See MFC-PropertySheets.cpp for the implementation of this class
+//
+
+class CMFCPropertySheetsApp : public CWinApp
+{
+public:
+	CMFCPropertySheetsApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCPropertySheetsApp theApp;
