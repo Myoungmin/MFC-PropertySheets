6956c43d47d715040832e668c545657618d03776
Myoungmin <miung222@gmail.com>
2022-09-21 20:49:00 +0900

    Add one more property page with ID IDD_PROPPAGE_2 and Caption Property Page 2

--- Resource.h
+++ Resource.h
@@ -8,13 +8,15 @@
 #define IDD_MFCPROPERTYSHEETS_DIALOG    102
 #define IDD_PROPPAGE_LARGE              107
 #define IDD_PROPPAGE_1                  107
+#define IDD_PROPPAGE_LARGE1             108
+#define IDD_PROPPAGE_2                  108
 #define IDR_MAINFRAME                   128
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
